8d7a002fadf9339bfee92cac67a54ee975793a39
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-10-29 11:33:05 +0100

    Remove more stuff

--- bcbio_monitor/static/js/monitor.js
+++ bcbio_monitor/static/js/monitor.js
@@ -1,17 +1,1 @@
-// Initialize mermeid stuff
-
-var mermaid_config = {
-    startOnLoad: true,
-    flowchart:  {
-        useMaxWidth:true,
-        htmlLabels:true,
-        logLevel: 1,
-        heigh: 1000
-    }
-}
-
-function callback_null(){
-    // Does nothing
-}
-
-mermaid.initialize(mermaid_config);
+// Custom JS methods and utils for bcbio-nextgen-monitor
