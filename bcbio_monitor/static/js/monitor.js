65e4d742c779090bdf6a70259b8af2d193acbd51
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-10-29 15:18:00 +0100

    Draw complete chard from log file

--- bcbio_monitor/static/js/monitor.js
+++ bcbio_monitor/static/js/monitor.js
@@ -6,9 +6,11 @@ function update_flowchart() {
     $.getJSON("/api/graph", function(data){
         var fc_viz = Viz(data['graph_data'], options={ format:"svg", engine:"dot" });
         var parser = new DOMParser();
-        var fc_svg = parser.parseFromString(fc_viz, "image/svg+xml");
+        var fc_svg = parser.parseFromString(fc_viz, "image/svg+xml").children[0];
+        fc_svg.style.display = "block";
+        fc_svg.style.margin = "auto";
         var div_graph = $('#progress_graph')[0];
-        div_graph.appendChild(fc_svg.children[0]);
+        div_graph.appendChild(fc_svg);
     });
 }
 
