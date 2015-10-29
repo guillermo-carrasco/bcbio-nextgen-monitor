658984cff560af63b98d3f4cc691dfa2a2ca8d8a
Guillermo Carrasco <guille.ch.88@gmail.com>
2015-10-29 14:00:04 +0100

    Generate graphs with graphviz. Fixes #11

--- bcbio_monitor/graph/__init__.py
+++ bcbio_monitor/graph/__init__.py
@@ -1,1 +1,15 @@
-"""Encapsulate graph functionality for the frontend"""
+"""Module to generate and work with process flowcharts"""
+
+from graphviz import Digraph
+
+
+class BcbioFlowChart(Digraph):
+    """Representation for a graphviz bcbio-nextgen flowchart"""
+
+    def __init__(self, name='bcbio-nextgen-flowchart', data=None):
+        super(BcbioFlowChart, self).__init__(comment='bcbio flow chart', format='png', encoding='UTF8')
+        self.name = name
+        if data:
+            # Not doing anything with the timestamps by the moment
+            for step, timestamp in data.iteritems():
+                self.node('_'.join(step.lower().split()), step)
--- bcbio_monitor/static/js/monitor.js
+++ bcbio_monitor/static/js/monitor.js
@@ -1,1 +1,12 @@
 // Custom JS methods and utils for bcbio-nextgen-monitor
+
+// Viz/graphviz utils
+
+/* Example for filling in graph data
+var result = Viz("digraph g { a -> b; }", options={ format:"svg", engine:"dot" });
+p = new DOMParser()
+var svg = p.parseFromString(result, "image/svg+xml");
+
+var div_graph = $('#progress_graph')[0]
+div_graph.appendChild(svg.children[0])
+*/
